89231a67183e7f665d73fd34137ce7f4b5d1f171
xsxiaosa <xsxiaosa@gmail.com>
2020-05-31 17:40:37 +0800

    style(code ): Add Code Line

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,50 @@
 module.exports = {
-    title: 'Vue3.0 源码解读',
-    description: 'Vue3.0 源码解读',
-    base: '/slamdunk-the-vue3/',
-    head: [
-        ['link', { rel: 'icon', href: '/onepunch.jpeg' }],
-        ['script', { src: 'https://hm.baidu.com/hm.js?4484bd6412288feacc311fd7f2054116'}]
+  title: "Vue3.0 源码解读",
+  description: "Vue3.0 源码解读",
+  base: "/slamdunk-the-vue3/",
+  head: [
+    ["link", { rel: "icon", href: "/onepunch.jpeg" }],
+    [
+      "script",
+      { src: "https://hm.baidu.com/hm.js?4484bd6412288feacc311fd7f2054116" },
     ],
-    themeConfig: {
-        nav: [
-            { text: 'Vue 3.0 解读', link: '/main/' },
-            { text: '关于我', link: '/about'},
-            { text: 'Github', link: 'https://github.com/hkc452/slamdunk-the-vue3' },
-        ],
-        sidebar: {
-            '/main/': [
-                {
-                    title: '响应式系统',
-                    // path: '/main/vue/reactivity',
-                    collapsable: false,
-                    children: [
-                        ['vue/reactivity/effect', 'effect'],
-                        ['vue/reactivity/reactive', 'reactive'],
-                        ['vue/reactivity/baseHandlers', 'baseHandlers'],
-                        ['vue/reactivity/collectionHandlers', 'collectionHandlers'],
-                        ['vue/reactivity/ref', 'ref'],
-                        ['vue/reactivity/computed', 'computed'],
-                    ]
-                },
-                {
-                    title: '编译模块',
-                    collapsable: false,
-                    // path: '/main/vue/compiler/',
-                    children: [
-                        ['vue/compiler/compiler', 'compiler'],
-                        ['vue/compiler/parse', 'parse'],
-                        ['vue/compiler/transform', 'transform(准备开始施工🚧)'],
-                        ['vue/compiler/codegen', 'codegen'],
-                    ]
-                }
-            ],
-        }
-    }
-}
+  ],
+  themeConfig: {
+    nav: [
+      { text: "Vue 3.0 解读", link: "/main/" },
+      { text: "关于我", link: "/about" },
+      { text: "Github", link: "https://github.com/hkc452/slamdunk-the-vue3" },
+    ],
+    sidebar: {
+      "/main/": [
+        {
+          title: "响应式系统",
+          // path: '/main/vue/reactivity',
+          collapsable: false,
+          children: [
+            ["vue/reactivity/effect", "effect"],
+            ["vue/reactivity/reactive", "reactive"],
+            ["vue/reactivity/baseHandlers", "baseHandlers"],
+            ["vue/reactivity/collectionHandlers", "collectionHandlers"],
+            ["vue/reactivity/ref", "ref"],
+            ["vue/reactivity/computed", "computed"],
+          ],
+        },
+        {
+          title: "编译模块",
+          collapsable: false,
+          // path: '/main/vue/compiler/',
+          children: [
+            ["vue/compiler/compiler", "compiler"],
+            ["vue/compiler/parse", "parse"],
+            ["vue/compiler/transform", "transform(准备开始施工🚧)"],
+            ["vue/compiler/codegen", "codegen"],
+          ],
+        },
+      ],
+    },
+  },
+  markdown: {
+    lineNumbers: true,
+  },
+};
