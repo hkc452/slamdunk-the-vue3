5d33f182801c64be7e71cfb6377d68a2ba215074
hkc <779543729@qq.com>
2020-06-09 15:17:43 +0800

    添加评论

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -53,4 +53,18 @@ module.exports = {
     markdown: {
         lineNumbers: true,
     },
+    plugins: [
+      [
+        'vuepress-plugin-comment',
+        {
+          choosen: 'valine',
+          // options选项中的所有参数，会传给Valine的配置
+          options: {
+            el: '#valine-vuepress-comment',
+            appId: 'R4Jdw58HGA1qQw6aSdWb120E-gzGzoHsz',
+            appKey: 'ME2oUMuVPyPpcsJitmBVGNJk'
+          }
+        }
+      ]
+    ]
 }
